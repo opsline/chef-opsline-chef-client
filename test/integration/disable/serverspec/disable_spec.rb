7cf4afa068fdf6604ac6be8d4af1142dfe1834a7
Roshan Singh <roshan@opsline.com>
2014-06-11 15:30:00 -0400

    added kitchen tests

---
+++ test/integration/disable/serverspec/disable_spec.rb
@@ -0,0 +1,14 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/sbin:/usr/sbin'
+  end
+end
+
+describe file('/var/log/chef/disabled') do
+  it { should be_file}
+end
