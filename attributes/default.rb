47c76539a8c1e8e317030079ee6d5949365c0dca
gpjonesii <gregory.jonesii@gmail.com>
2014-11-03 12:41:40 -0500

    added support for passing json attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,5 @@ default['opsline-chef-client']['runs_per_hour'] = 2
 default['opsline-chef-client']['logrotate']['enabled'] = true
 default['opsline-chef-client']['logrotate']['days'] = 30
 default['opsline-chef-client']['log_file'] = '/var/log/chef/client.log'
+default['opsline-chef-client']['json_attributes_file'] = nil
 default['opsline-chef-client']['unregister_at_shutdown'] = false
